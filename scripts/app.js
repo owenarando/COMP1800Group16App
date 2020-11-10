177a82fa701ac197831909cd7446193e5cc893c3
Stan Liu <stankky1990@gmail.com>
2020-11-09 23:27:19 -0800

    API fixed/ Rearrange doc locations

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
+
 function readQuote(){
-  db.collection("quotes").doc("01")
-  .onSnapshot(function(snap){
-    console.log(snap.data()); //print out the document fields of "01"
-    console.log(snap.data().message); //spelled Exactly as the firestore console
-    document.getElementById("abc")
-    document.getElementById("abc").innerText = snap.data().message;
-  })
+    db.collection("quotes").doc("01")
+    .onSnapshot(function(snap){
+        console.log(snap.data()); //print the document fields of "01" in console
+        console.log(snap.data().message); //spelled the same sa the "message" in firestore
+        document.getElementById("a").innerText = snap.data().message;
+    })
 }
 
 readQuote();
