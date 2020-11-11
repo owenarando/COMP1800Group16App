a8d161cc25a805d581a7cc0a9148021f817470a0
Gavin Knoke <gavink1279@hotmail.com>
2020-11-10 18:13:59 -0800

    Added threadCreation and style, and javascript for database

---
+++ scripts/threadCreation.js
@@ -0,0 +1,21 @@
+function addListener() {
+
+  //Waits for the submit button to be pressed
+  document.getElementById("object").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    //Getting the html elements from threadCreation.html
+    var title = document.getElementById("inputTitle").value;
+    var body = document.getElementById("inputBody").value;
+
+    //Navigating to the thread in database
+    db.collection("group").doc("FeQvXsEC226j9IJegGRb").collection("thread")
+      .add({
+        "title": title,
+        "description": body
+      })
+  })
+}
+
+addListener();
+
